07bdd05c73fa00f946bdd693203a6fb4f507c28c
Andreas Hennings <andreas@dqxtech.net>
2018-03-21 19:36:10 +0100

    #62: Add ThemeHookStub and ThemeHookStubFactory.

---
+++ src/ThemeHookStub.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Drupal\registryonsteroids;
+
+/**
+ * Contains data to build a modified theme registry entry.
+ */
+class ThemeHookStub {
+
+  /**
+   * @var string
+   */
+  private $baseHook;
+
+  /**
+   * @var self
+   */
+  private $baseHookStub;
+
+  /**
+   * @var string[]
+   *   Format: [$base_hook, .., $hook]
+   */
+  private $cascade;
+
+  /**
+   * @var array
+   */
+  private $info;
+
+  /**
+   * @var string[][]
+   *   Format: $[$phase_key][$weight][] = $placeholder_or_function
+   */
+  private $placeholderssByPhasekeyAndWeight = array(
+    'process functions' => array(),
+    'preprocess functions' => array(),
+  );
+
+  /**
+   * @var string[][]
+   *   Format: $[$phase_key]['@' . $function] = $function
+   */
+  private $replacementssByPhasekey = array(
+    'process functions' => array(),
+    'preprocess functions' => array(),
+  );
+
+  /**
+   * @param string $hook
+   * @param array $info
+   * @param string[][] $functionsByPhasekeyAndWeight
+   *   Format: $[$phase_key][$weight] = $function
+   * @param string[][] $templateFunctionsByPhasekeyAndWeight
+   *   Format: $[$phase_key][$weight] = $function
+   *   Should be empty if this is not a template.
+   *
+   * @return self
+   */
+  public static function createRoot($hook, array $info, array $functionsByPhasekeyAndWeight, array $templateFunctionsByPhasekeyAndWeight) {
+    $root = new self();
+    $root->baseHook = $hook;
+    $root->cascade = array($hook);
+    $root->baseHookStub = $root;
+    $root->info = $info;
+    foreach ($templateFunctionsByPhasekeyAndWeight as $phase_key => $functionsByWeight) {
+      foreach ($functionsByWeight as $weight => $function) {
+        $root->placeholderssByPhasekeyAndWeight[$phase_key][$weight][] = $function;
+      }
+    }
+    foreach ($functionsByPhasekeyAndWeight as $phase_key => $functionsByWeight) {
+      foreach ($functionsByWeight as $weight => $function) {
+        $root->placeholderssByPhasekeyAndWeight[$phase_key][$weight][] = $function;
+      }
+    }
+    return $root;
+  }
+
+  /**
+   * Private (incomplete) constructor.
+   */
+  private function __construct() {}
+
+  /**
+   * @param string $hook
+   * @param array|null $info
+   * @param array $functionsByPhasekeyAndWeight
+   *
+   * @return static
+   */
+  public function addVariant($hook, array $info = NULL, array $functionsByPhasekeyAndWeight) {
+    $variant = clone $this;
+    $variant->cascade[] = $hook;
+    foreach ($functionsByPhasekeyAndWeight as $phase_key => $functionsByWeight) {
+      foreach ($functionsByWeight as $weight => $function) {
+        $this->baseHookStub->placeholderssByPhasekeyAndWeight[$phase_key][$weight][] = '@' . $function;
+        $variant->replacementssByPhasekey[$phase_key]['@' . $function] = $function;
+      }
+    }
+    if (NULL !== $info) {
+      $variant->info = $info;
+    }
+    else {
+      // Do not inherit processor functions yet. This happens later.
+      unset($variant->info['process functions']);
+      unset($variant->info['preprocess functions']);
+    }
+    $variant->info['base hook'] = $this->baseHook;
+    $variant->placeholderssByPhasekeyAndWeight = array();
+    return $variant;
+  }
+
+  /**
+   * @return array
+   */
+  public function getRegistryEntry() {
+    $info = $this->info;
+    foreach ($this->getPlaceholdersByPhasekeySorted() as $phase_key => $placeholders_sorted) {
+      $info[$phase_key] = $placeholders_sorted;
+    }
+
+    $info['registryonsteroids replace'] = $this->replacementssByPhasekey;
+
+    return $info;
+  }
+
+  /**
+   * @return string[][][]
+   *   Format: $[$phase_key][] = $function_or_placeholder
+   */
+  private function getPlaceholdersByPhasekeySorted() {
+
+    $placeholders_by_phasekey = array();
+    foreach ($this->placeholderssByPhasekeyAndWeight as $phase_key => $placeholderss_by_weight) {
+      ksort($placeholderss_by_weight);
+      $placeholders_by_phasekey[$phase_key] = array() !== $placeholderss_by_weight
+        ? array_merge(...$placeholderss_by_weight)
+        : array();
+    }
+
+    return $placeholders_by_phasekey;
+  }
+
+}
---
+++ src/ThemeHookStubFactory.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\registryonsteroids;
+
+/**
+ * Creates ThemeHookStub objects.
+ */
+class ThemeHookStubFactory {
+
+  /**
+   * @var array[]
+   */
+  private $registry;
+
+  /**
+   * @var string[][][]
+   *   Format: $[$hook][$phase_key][$weight] = $function
+   */
+  private $functionsByHookAndPhasekeyAndWeight;
+
+  /**
+   * @var string[][]
+   *   Format: $[$phase_key][$weight] = $function
+   */
+  private $templateFunctionsByPhasekeyAndWeight;
+
+  /**
+   * @param array[] $registry
+   * @param string[][][] $functions_by_hook_and_phasekey_and_weight
+   */
+  public function __construct(array $registry, array $functions_by_hook_and_phasekey_and_weight) {
+    $this->registry = $registry;
+    $this->functionsByHookAndPhasekeyAndWeight = $functions_by_hook_and_phasekey_and_weight;
+    $this->templateFunctionsByPhasekeyAndWeight = $functions_by_hook_and_phasekey_and_weight['*'];
+  }
+
+  /**
+   * @param string $hook
+   * @param \Drupal\registryonsteroids\ThemeHookStub|null $parent
+   *
+   * @return \Drupal\registryonsteroids\ThemeHookStub|null
+   */
+  public function createStub($hook, ThemeHookStub $parent = NULL) {
+
+    $info = isset($this->registry[$hook])
+      ? $this->registry[$hook]
+      : NULL;
+
+    $functions_by_phasekey_and_weight = isset($this->functionsByHookAndPhasekeyAndWeight[$hook])
+      ? $this->functionsByHookAndPhasekeyAndWeight[$hook]
+      : array();
+
+    if (NULL !== $parent) {
+      return $parent->addVariant(
+        $hook,
+        $info,
+        $functions_by_phasekey_and_weight);
+    }
+
+    if (NULL === $info) {
+      return NULL;
+    }
+
+    return ThemeHookStub::createRoot(
+      $hook,
+      $info,
+      $functions_by_phasekey_and_weight,
+      isset($info['template'])
+        ? $this->templateFunctionsByPhasekeyAndWeight
+        : array());
+  }
+
+}
